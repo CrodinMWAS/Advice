6a49ead7a3d92db64dfa1a7ed35b1b46271bb274
CrodinMWAS <MWASiskola@gmail.com>
2024-11-21 08:58:55 +0100

    update

--- src/Components/Advice.tsx
+++ src/Components/Advice.tsx
@@ -1,13 +1,38 @@
 import dividerMobile from "../../images/pattern-divider-mobile.svg"
 import dividerDesktop from "../../images/pattern-divider-desktop.svg"
 import dice from "../../images/icon-dice.svg"
+import { useEffect, useState } from "react"
+
+type AdviceType = {
+    id:number,
+    advice:string
+}
 
 const Advice = () => {
+    const [advice, setAdvice] = useState("")
+
+    const fetchAdvice = async()=>{
+        try {
+            const response = await fetch("https://api.adviceslip.com/advice")
+            if (!response.ok) {
+                throw new Error
+            }
+            const data = await response.json()
+            console.log(data)
+            setAdvice(data.slip)
+        } catch (error) {
+            console.error("error:", error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchAdvice()
+    },[])
     
   return (
     <div className="card">
-        <p>Advice #1</p>
-        <h2>Lingan guli guli</h2>
+        <p>Advice #{advice?.id}</p>
+        <h2>{advice?.advice}</h2>
         <img src={dividerDesktop} className="dividerDesktop" alt="" />
         <img src={dividerMobile} className="dividerMobile" alt="" />
         <div className="dice">
